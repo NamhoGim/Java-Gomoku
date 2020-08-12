6d6e2b924651f19e7a018454d49278a10358c36b
NamhoGim <kkjknh2@gmail.com>
2020-08-12 20:10:58 +0900

    feat: more comfortable to read board printStatus function implemented

--- Gomoku.java
+++ Gomoku.java
@@ -20,9 +20,19 @@ public class Gomoku implements Simulatable, Winnable, Playable, Printable {
     @Override
     public void printStatus() {
         StringBuilder stringBuilder = new StringBuilder();
-        for (char[] chars : board) {
+        for (int j = 0; j <= board[0].length; j++) {
+            if (j == 0) {
+                stringBuilder.append("   ");
+            } else {
+                stringBuilder.append(String.format("%2d", j-1)).append(" ");
+            }
+        }
+        stringBuilder.append('\n');
+
+        for (int i = 0; i < board.length; i++) {
+            stringBuilder.append(String.format("%2d", i)).append(" ");
             for (int j = 0; j < board[0].length; j++) {
-                stringBuilder.append(chars[j]);
+                stringBuilder.append(" ").append(board[i][j]).append(" ");
             }
             stringBuilder.append('\n');
         }
@@ -31,7 +41,11 @@ public class Gomoku implements Simulatable, Winnable, Playable, Printable {
 
     @Override
     public void initialize() {
+        System.out.println("Put player1's name: ");
+        gomoku.setPlayer1(new Player());
 
+        System.out.println("Put player2's name: ");
+        gomoku.setPlayer2(new Player());
     }
 
     @Override
@@ -43,9 +57,11 @@ public class Gomoku implements Simulatable, Winnable, Playable, Printable {
     public void reset() {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                board[i][j] = ' ';
+                board[i][j] = '.';
             }
         }
+        gomoku.getPlayer1().setNumWin(0);
+        gomoku.getPlayer2().setNumWin(0);
     }
 
     @Override
