e6d12ac7aa0cfa3616c6ab6301738616c7ced010
NamhoGim <kkjknh2@gmail.com>
2020-08-12 20:13:51 +0900

    fix: check position whether the go stone was located before

--- Player.java
+++ Player.java
@@ -19,29 +19,48 @@ public class Player implements Inputtable {
         return name;
     }
 
+    public int getNumWin() {
+        return numWin;
+    }
+
+    public void setNumWin(int val) {
+        numWin = 0;
+    }
+
     @Override
     public void getKeyboardInput() {
         int M = Gomoku.board.length;
         int N = Gomoku.board[0].length;
         int x = -1, y = -1;
+        boolean isInvalid = false;
         while (!((x >= 0 && x < M) && (y >= 0 && y < N))) {
+            if (isInvalid) {
+                System.out.println("Please put valid coordinate:");
+            }
             String line = scan.nextLine();
-            System.out.println(line);
             String[] split = line.split(" ");
             if ((split[0].matches("^[qQ]$"))) {
                 Gomoku.isQuit = true;
                 return;
             } else {
                 if (split.length > 2) {
+                    isInvalid = true;
                     continue;
                 } else if (split.length == 1 && !split[0].matches("[0-9]+")) {
+                    isInvalid = true;
                     continue;
                 } else if (split.length == 2 && (!split[0].matches("[0-9]+") || !split[1].matches("[0-9]+"))) {
+                    isInvalid = true;
                     continue;
                 }
                 x = Integer.parseInt(split[0]);
                 y = Integer.parseInt(split[1]);
-
+                if (Gomoku.board[x][y] != '.') {
+                    x = -1;
+                    y = -1;
+                    isInvalid = true;
+                    continue;
+                }
                 Gomoku.board[x][y] = (this == Gomoku.getInstance().getPlayer1()) ? 'O' : 'X';
             }
         }
