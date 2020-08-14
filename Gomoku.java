989738e593edf1fa6038847e3a42673bebf952d9
NamhoGim <kkjknh2@gmail.com>
2020-08-14 11:38:13 +0900

    feat: winCondition is implemented

--- Gomoku.java
+++ Gomoku.java
@@ -2,8 +2,10 @@ public class Gomoku implements Simulatable, Winnable, Playable, Printable {
     private static Gomoku gomoku = new Gomoku();
 
     static boolean isQuit = false;
-    private Player player1, player2;
     static char[][] board = new char[15][15];
+    static private Position[] directions = new Position[]{new Position(0, -1), new Position(-1, -1),
+                                                    new Position(1, 0), new Position(-1, 1)};
+    private Player player1, player2;
 
     private Gomoku() {}
 
@@ -13,12 +15,24 @@ public class Gomoku implements Simulatable, Winnable, Playable, Printable {
 
     @Override
     public void play(Player player, Position pos) {
+        printStatus();
         player.getKeyboardInput();
         if (Gomoku.isQuit) {
             isFinished();
             return;
         }
-        printStatus();
+        if (threeByThreeCondition()) {
+            if (player == gomoku.player2) {
+               gomoku.player1.setNumWin(gomoku.player1.getNumWin()+1);
+            } else {
+                gomoku.player2.setNumWin(gomoku.player2.getNumWin()+1);
+            }
+        }
+        if (winCondition(pos)) {
+            System.out.println(player.getName() + "'s Win!!!");
+            player.setNumWin(player.getNumWin()+1);
+            reset();
+        }
     }
 
     @Override
@@ -56,6 +70,12 @@ public class Gomoku implements Simulatable, Winnable, Playable, Printable {
     public void isFinished() {
         System.out.println(player1.getName() + " " + player1.getNumWin() +
                 " - " + player2.getName() + " " + player2.getNumWin());
+        if (getWinner() != null) {
+            System.out.println(getWinner().getName() + " is Winner!, Congrats!!!");
+        } else {
+            System.out.println("Draw");
+        }
+        System.out.println("-- The End. --");
     }
 
     @Override
@@ -65,13 +85,42 @@ public class Gomoku implements Simulatable, Winnable, Playable, Printable {
                 board[i][j] = '.';
             }
         }
-        gomoku.getPlayer1().setNumWin(0);
-        gomoku.getPlayer2().setNumWin(0);
     }
 
     @Override
     public Player getWinner() {
-        return null;
+        return (player1.getNumWin() > player2.getNumWin()) ? player1 : ((player1.getNumWin() == player2.getNumWin()) ? null : player2);
+    }
+
+    private boolean boundaryCheck(int x, int y) {
+        return ((x >= 0 && x < Gomoku.board.length) && (y >= 0 && y < Gomoku.board[0].length));
+    }
+
+    private boolean winCondition(Position p) {
+        System.out.println(p.getX() + " " + p.getY());
+        int maxCount = 0;
+        int px = p.getX(), py = p.getY();
+        char stone = Gomoku.board[px][py];
+
+        for (Position d: directions) {
+            int count = 1;
+            int dx = d.getX(), dy = d.getY();
+            for (int cx = px+dx, cy = py+dy; boundaryCheck(cx, cy) &&
+                    (stone == Gomoku.board[cx][cy]); cx += dx, cy += dy) {
+                if (stone == Gomoku.board[cx][cy]) count++;
+            }
+
+            for (int cx = px-dx, cy = py-dy; boundaryCheck(cx, cy) &&
+                    (stone == Gomoku.board[cx][cy]); cx -= dx, cy -= dy) {
+                if (stone == Gomoku.board[cx][cy]) count++;
+            }
+            maxCount = Math.max(maxCount, count);
+        }
+        return maxCount == 5;
+    }
+
+    private boolean threeByThreeCondition() {
+        return false;
     }
 
     public void setPlayer1(Player player1) {
