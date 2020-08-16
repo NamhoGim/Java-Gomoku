20a1b9144a347c7e5aed5288c226f8212f3f36ad
NamhoGim <kkjknh2@gmail.com>
2020-08-16 22:31:03 +0900

    33 check logic currently buggy should fix bug which triggering 33 on (3, 3)

--- Gomoku.java
+++ Gomoku.java
@@ -1,10 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
 
 public class Gomoku implements Simulatable, Winnable, Playable, Printable {
     private static final Gomoku gomoku = new Gomoku();
     private static final Position[] directions = new Position[]{new Position(0, -1), new Position(-1, -1),
-                                                                new Position(1, 0), new Position(-1, 1)};
+                                                                new Position(-1, 0), new Position(1, -1)};
     private static final String[] pattern1 = new String[]{".OOO..", "..OOO.", ".O.OO.", ".OO.O."};
     private static final String[] pattern2 = new String[]{".XXX..", "..XXX.", ".X.XX.", ".XX.X."};
 
@@ -28,12 +27,22 @@ public class Gomoku implements Simulatable, Winnable, Playable, Printable {
             return;
         }
 
-        if (threeByThreeCondition(pos)) {
-            if (player == gomoku.player2) {
-               gomoku.player1.setNumWin(gomoku.player1.getNumWin()+1);
+        if (threeByThreeCondition()) {
+            printStatus();
+            System.out.println("33 입니다.");
+
+            Player winner;
+            if (player == player2) {
+                winner = player1;
+                player1.setNumWin(player1.getNumWin()+1);
             } else {
-                gomoku.player2.setNumWin(gomoku.player2.getNumWin()+1);
+                winner = player2;
+                player2.setNumWin(player2.getNumWin()+1);
             }
+            System.out.println(winner.getName() + "의 승리 !!!");
+            System.out.println(winner.getNumWin() + "번 이겼다!");
+            reset();
+            return;
         }
 
         if (winCondition(pos)) {
@@ -99,11 +108,12 @@ public class Gomoku implements Simulatable, Winnable, Playable, Printable {
 
     @Override
     public Player getWinner() {
-        return (player1.getNumWin() > player2.getNumWin()) ? player1 : ((player1.getNumWin() == player2.getNumWin()) ? null : player2);
+        if (player1.getNumWin() > player2.getNumWin()) return player1;
+        return (player1.getNumWin() == player2.getNumWin()) ? null : player2;
     }
 
     private boolean boundaryCheck(int x, int y) {
-        return ((x >= 0 && x < Gomoku.board.length) && (y >= 0 && y < Gomoku.board[0].length));
+        return ((x >= 0 && x < board.length) && (y >= 0 && y < board[0].length));
     }
 
     private boolean winCondition(Position p) {
@@ -113,66 +123,171 @@ public class Gomoku implements Simulatable, Winnable, Playable, Printable {
     private boolean checkFiveSequence(Position p) {
         int maxCount = 0;
         int px = p.getX(), py = p.getY();
-        char stone = Gomoku.board[px][py];
+        char stone = board[px][py];
 
         for (Position d : directions) {
             int count = 1;
             int dx = d.getX(), dy = d.getY();
             for (int cx = px + dx, cy = py + dy; boundaryCheck(cx, cy) &&
-                    (stone == Gomoku.board[cx][cy]); cx += dx, cy += dy) {
-                if (stone == Gomoku.board[cx][cy]) count++;
+                    (stone == board[cx][cy]); cx += dx, cy += dy) {
+                if (stone == board[cx][cy]) count++;
             }
 
             for (int cx = px - dx, cy = py - dy; boundaryCheck(cx, cy) &&
-                    (stone == Gomoku.board[cx][cy]); cx -= dx, cy -= dy) {
-                if (stone == Gomoku.board[cx][cy]) count++;
+                    (stone == board[cx][cy]); cx -= dx, cy -= dy) {
+                if (stone == board[cx][cy]) count++;
             }
             maxCount = Math.max(maxCount, count);
         }
         return maxCount == 5;
     }
 
-    private boolean threeByThreeCondition(Position currPos) {
-        //for (Position d1: directions) {
-        //    List<List<Position>> list = getOpenThreeWithSpace(currPos, d1);
-        //    if (!list.isEmpty()) {
-        //        for (Position pivot : list) {
-        //            for (Position d2 : directions) {
-        //                if (!d2.equals(d1)) {
-        //                    List<Position> result = getOpenThreeWithSpace(pivot, d2);
-        //                    if (!result.isEmpty()) {
-        //                        return true;
-        //                    }
-        //                }
-        //            }
-        //        }
-        //    }
-        //}
+    private boolean threeByThreeCondition() {
         return false;
     }
 
-    private boolean diffStone(char stone, int x, int y) {
-        return (Gomoku.board[x][y] != '.') && (Gomoku.board[x][y] != stone);
+    private boolean threeByThreeCondition(Position currPos) {
+        for (Position d1: directions) {
+            HashSet<Position> set = getOpenThree(currPos, d1);
+            if (set.size() == 0) {
+                continue;
+            }
+            System.out.println("check!");
+            for (Position pos : set) {
+                System.out.println(pos);
+                for (Position d2 : directions) {
+                    if (d1.getX() != d2.getX() || d1.getY() != d2.getY()) {
+                        if (isIncludedOpenThree(currPos, pos, d2)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 
-    private List<Position> getOpenThreeWithSpace(Position p, Position d) {
-    //    List<List<Position>> result = new ArrayList<>();
+    private HashSet<Position> getOpenThree(Position pos, Position dir) {
+        HashSet<Position> result = new HashSet<>();
+        patternCollect(pos, dir, result);
+        return result;
+    }
 
-    //    boolean blank = p.isBlank();
-    //    int dx = d.getX(), dy = d.getY();
-    //    int x = p.getX(), y = p.getX();
-    //    char stone = Gomoku.board[x][y];
-    //    int i, j;
+    private boolean isIncludedOpenThree(Position pos, Position candi, Position dir) {
+        int x = pos.getX(), y = pos.getY();
+        String[] patterns = (board[x][y] == 'O') ? pattern1 : pattern2;
 
-    //    for (i = x + dx, j = y + dy; boundaryCheck(i, j); i += dx, j += dy) {
+        if (candi.isBlank()) { // EAEAAE, EAAEAE
+            if (patternMatch(candi, dir, patterns[2], 2) != null) {
+                return true;
+            } else return patternMatch(candi, dir, patterns[3], 3) != null;
+        }
 
-    //    }
+        for (int i = 0; i < 4; i++) {
+            if (patternMatch(candi, dir, patterns[i], i) != null) {
+                return true;
+            }
+        }
 
-    //    for (i = x - dx, j = y - dy; boundaryCheck(i, j); i -= dx, j -= dy) {
+        return false;
+    }
 
-    //    }
+    private void patternCollect(Position pos, Position dir, HashSet<Position> set) {
+        int x = pos.getX(), y = pos.getY();
+        String[] patterns = (board[x][y] == 'O') ? pattern1 : pattern2;
+        for (int i = 0; i < patterns.length; i++) {
+            Position left = patternMatch(pos, dir, patterns[i], i);
+            if (left != null) {
+                if (i == 0 || i == 1) {
+                    addToSet(set, left, dir, 3);
+                } else {
+                    addToSet(set, left, dir, 4);
+                }
+            }
+        }
+    }
 
-        return new ArrayList<>();
+    private Position patternMatch(Position pos, Position dir, String pattern, int i) {
+        Position result;
+        if (!pos.isBlank()) {
+            switch (i) {
+                //case 0: // EAAAEE
+                //    for (int j = 1; j <= 3; j++) {
+                //        if (compareWithPattern(pos, dir, pattern, j)) {
+                //            System.out.println(pattern);
+                //            result.setX(pos.getX() + dir.getX() * (j - 1));
+                //            result.setY(pos.getY() + dir.getY() * (j - 1));
+                //            return result;
+                //        }
+                //    }
+                //    break;
+                case 1: // EEAAAE
+                    for (int j = 2; j <= 4; j++) {
+                        if (compareWithPattern(pos, dir, pattern, j)) {
+                            result = new Position();
+                            result.setX(pos.getX() + dir.getX() * (j - 2));
+                            result.setY(pos.getY() + dir.getY() * (j - 2));
+                            return result;
+                        }
+                    }
+                    break;
+                //case 2: // EAEAAE
+                //    for (int j = 1; j <= 4; j++) {
+                //        if (j != 2 && compareWithPattern(pos, dir, pattern, j)) {
+                //            System.out.println(pattern);
+                //            result.setX(pos.getX() + dir.getX() * (j - 1));
+                //            result.setY(pos.getY() + dir.getY() * (j - 1));
+                //            return result;
+                //        }
+                //    }
+                //    break;
+                //case 3: // EAAEAE
+                //    for (int j = 1; j <= 4; j++) {
+                //        if (j != 3 && compareWithPattern(pos, dir, pattern, j)) {
+                //            System.out.println(pattern);
+                //            result.setX(pos.getX() + dir.getX() * (j - 1));
+                //            result.setY(pos.getY() + dir.getY() * (j - 1));
+                //            return result;
+                //        }
+                //    }
+                //    break;
+            }
+        }
+        //else {
+        //    if (i == 2 && compareWithPattern(pos, dir, pattern, 2)) {
+        //        System.out.println(pattern);
+        //        result.setX(pos.getX() + dir.getX());
+        //        result.setY(pos.getY() + dir.getY());
+        //        return result;
+        //    } else if (i == 3 && compareWithPattern(pos, dir, pattern, 3)) {
+        //        System.out.println(pattern);
+        //        result.setX(pos.getX() + dir.getX()*2);
+        //        result.setY(pos.getY() + dir.getY()*2);
+        //        return result;
+        //    }
+        //}
+        return null;
+    }
+
+    private boolean compareWithPattern(Position pos, Position dir, String pattern, int offset) {
+        System.out.println(pattern);
+        int x = pos.getX(), y = pos.getY();
+        int dx = dir.getX(), dy = dir.getY();
+        int cx = x + offset*dx, cy = y + offset*dy;
+        System.out.println("start: " + cx + ", " + cy);
+        for (int i = 0; boundaryCheck(cx, cy) && i < pattern.length(); cx -= dx, cy -= dy, i++) {
+            System.out.println(cx + ", " + cy + "," + board[cx][cy]);
+            if (board[cx][cy] != pattern.charAt(i)) return false;
+        }
+        return true;
+    }
+
+    private void addToSet(HashSet<Position> set, Position left, Position dir, int len) {
+        int x = left.getX(), y = left.getY();
+        int dx = dir.getX(), dy = dir.getY();
+        for (int i = 0; boundaryCheck(x, y) && i < len; i++, x -= dx, y -= dy ) {
+            set.add(new Position(x, y, (board[x][y] == '.')));
+        }
     }
 
     public void setPlayer1(Player player1) {
@@ -180,7 +295,7 @@ public class Gomoku implements Simulatable, Winnable, Playable, Printable {
     }
 
     public Player getPlayer1() {
-        return gomoku.player1;
+        return player1;
     }
 
     public void setPlayer2(Player player2) {
@@ -188,6 +303,6 @@ public class Gomoku implements Simulatable, Winnable, Playable, Printable {
     }
 
     public Player getPlayer2() {
-        return gomoku.player2;
+        return player2;
     }
 }
--- Position.java
+++ Position.java
@@ -1,14 +1,23 @@
 public class Position {
     private int x, y;
-    private boolean blank;
+    //private int d; // represents 4 directions as integer value, 0: (0, -1), 1: (-1, -1), 2: (-1, 0), 3: (1, -1)
+    private boolean blank; // indicates that current position is blank,
+                           // which means not being occupied by any gomoku stone
 
     public Position() {
-        this(0, 0);
+        this(0, 0, false);
     }
 
     public Position(int x, int y) {
         this.x = x;
         this.y = y;
+        this.blank = false;
+    }
+
+    public Position(int x, int y, boolean b) {
+        this.x = x;
+        this.y = y;
+        this.blank = b;
     }
 
     public int getX() {
@@ -25,5 +34,10 @@ public class Position {
     }
     public boolean isBlank() {
         return blank;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d, %s)", x, y, blank);
     }
 }
